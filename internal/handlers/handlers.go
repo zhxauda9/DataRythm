8b009834192cfeb220a75d8cbb15400590398732
zhxauda9 <zhalgasovaida@gmail.com>
2024-11-02 13:03:17 +0500

    added hadnlers for REST API

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func CreateOrder(c *gin.Context) {
+	// var newOrder o.Order
+	// if err := c.ShouldBindJSON(&newOrder); err == nil {
+	// 	orders = append(orders, newOrder)
+	// 	c.JSON(http.StatusCreated, newOrder)
+	// } else {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	// }
+}
+
+func GetOrders(c *gin.Context) {
+}
+
+func GetOrder(c *gin.Context) {
+}
+
+func UpdateOrder(c *gin.Context) {
+}
+
+func DeleteOrder(c *gin.Context) {
+}
